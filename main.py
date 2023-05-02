5eb74bace38a2c6c3f82cbd10ade12791e4f1906
spear <speardfire@gmail.com>
2023-05-02 15:58:28 +0200

    Init

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-import panda
+import pandas as pd
+pd.set_option('display.max_colwidth',5)
+ufo = pd.read_csv("scrubbed.csv",low_memory=False)
+date = ufo["datetime"]
+misdate = ufo[ufo["shape"].isna()]
+
+print(misdate)
