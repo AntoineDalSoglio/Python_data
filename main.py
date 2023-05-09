8fbb4b61fc8d01387c36c6cef31d05dd2db9667d
spear <speardfire@gmail.com>
2023-05-09 15:21:23 +0200

    09/05

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import pandas as pd
+
 pd.set_option('display.max_colwidth',5)
 ufo = pd.read_csv("scrubbed.csv",low_memory=False)
-date = ufo["datetime"]
-misdate = ufo[ufo["shape"].isna()]
 
-print(misdate)
+noshape = ufo.loc[ufo["shape"].isna()]
+
+for i in noshape.index:
+    print(i)
+    ufo.loc[i]["shape"] = "unknown"
+    print(ufo.loc[i]["shape"])
+
+#print(noshape["shape"])
